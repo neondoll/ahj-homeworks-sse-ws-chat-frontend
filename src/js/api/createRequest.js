e997995b38a662a71b5272de0e9fae2e8a9458c9
neondoll <bonni_1998@mail.ru>
2024-07-24 00:35:57 +0300

    first commit

---
+++ src/js/api/createRequest.js
@@ -0,0 +1,14 @@
+const createRequest = async (options) => {
+  const init = {
+    headers: { 'Content-Type': 'application/json' },
+    method: options.method,
+  };
+
+  if (options.method === 'POST' || options.method === 'PUT') {
+    init.body = JSON.stringify(options.body);
+  }
+
+  return await fetch('http://localhost:3000' + options.url, init);
+};
+
+export default createRequest;
